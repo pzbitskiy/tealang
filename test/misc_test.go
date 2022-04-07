6a5256e8a891835ee02ceb5d57b41283398f750d
Pavel Zbitskiy <pavel@algorand.com>
2022-04-06 21:46:07 -0400

    Add bsqrt, divw

--- test/misc_test.go
+++ test/misc_test.go
@@ -12,6 +12,7 @@ function logic() {
 	let r = band(z, "\x11");
 	assert(r == "\x00\x00\x00\x00")
 	assert(badd("\x01\x02\x03\x04", z) == "\x01\x02\x03\x04")
+	assert(bsqrt("\x11") == "\x04")
 	return 1;
 }
 `
@@ -52,6 +53,20 @@ function logic() {
 	a = substring("\x12\x34\x56\x78\x9a\xbc", s, e)
 	assert(a == "\x34\x56")
 
+	return 1
+}
+`
+	performTest(t, source)
+}
+
+func TestDivw(t *testing.T) {
+	source := `
+function logic() {
+	let a = divw(1, 0, 2)
+	assert(a == 0x8000000000000000)
+
+	a = divw(0, 90, 30)
+	assert(a == 3)
 	return 1
 }
 `
